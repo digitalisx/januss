a3ccdbcecf6b54e60d7f113ab1a1ddad7ada0088
Donghyun Kim <digitalisx99@gmail.com>
2022-05-03 15:33:48 +0900

    Add: all, plugin commands

--- janus.py
+++ janus.py
@@ -1,4 +1,17 @@
-import janus.cli
+import click
+
+
+class janus:
+    @click.command()
+    @click.option("--all", default=False, prompt="Your name", help="The person to greet.")
+    @click.option("--plugin", prompt="", help="")
+    def run():
+        """The Framework that Malware Analysis by Memory Comparison"""
+
+        #TODO
+        # We need to dynamically invoke volatility3 plugin objects through parameters.
+
+        return
 
 if __name__ == "__main__":
-    janus.cli.main()
+    janus.run()
